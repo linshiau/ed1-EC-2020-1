c9bc4f54f32719217718b2e71de214259c47b6b0
linshiau <shiaulon@hotmail.com>
2020-11-08 17:12:06 -0300

    corrigindo menu e adicionando soma de matrizes

--- menuProjeto.c
+++ menuProjeto.c
@@ -7,7 +7,7 @@ int opcao;
     inicio:
     system("cls");
     system("color 0F");
-    printf("1 - multiplicacao de matrizes \n2 - soma de matrizes \n3 - esaclonamento de matriz \n4 - matriz inversa\n");
+    printf("1 - MULTIPLICACAO DE MATRIZES \n2 - SOMA DE MATRIZES \n3 - ESCALONAMENTO DE MATRIZ \n4 - MATRIZ INVERSA\n");
     opcao = getch();
     switch (opcao) {
     case '1':
@@ -87,7 +87,72 @@ int opcao;
         break;
     case '2':
         system("cls");
-        printf("Ranking\n");
+
+        printf("\n==========SOMA DE MATRIZES==========\n");
+              printf("\nMatriz 1\n==========\n");
+              printf("\nDigite o numero de linhas...: ");
+              scanf("%d", &i1);
+              printf("Digite o numero de colunas..: ");
+              scanf("%d", &j1);
+              printf("\nMatriz 2\n==========\n");
+              printf("\nDigite o numero de linhas...: ");
+              scanf("%d", &i2);
+              printf("Digite o numero de colunas..: ");
+              scanf("%d", &j2);
+              if (j1 == i2) {
+                printf("\nValores da Matriz 1\n=====================\n\n");
+                for (i=0; i<i1; i++) {
+                  for(j=0; j<j1; j++) {
+                    printf("Digite um valor para [%d][%d].: ", (i+1), (j+1));
+                    scanf("%d", &m1[i][j]);
+
+                  }
+
+                }
+
+                printf("\nValores da Matriz 2\n=====================\n\n");
+                for (i=0; i<i2; i++) {
+                  for (j=0; j<j2; j++) {
+                    printf("Digite um valor para [%d][%d].: ", (i+1), (j+1));
+                    scanf("%d", &m2[i][j]);
+
+                  }
+
+                }
+
+                for (i=0;i<i1; i++) {
+                  for (j=0; j<i1; j++) {
+
+                      m3[i][j] = m3[i][j] + (m1[i][j] + m2[i][j]);
+
+
+
+                  }
+
+                }
+
+                printf("\nMatriz 3\n=====================\n\n");
+
+                for (i=0; i<i1; i++) {
+                  for (j=0; j<j2; j++) {
+                    printf("%d ", m3[i][j]);
+
+                  }
+
+                  printf("\n");
+
+                }
+
+                printf("\n");
+
+              }
+
+              else {
+                  printf("\nErro! Impossivel somar as matrizes informadas.\n");
+
+                }
+
+              return(0);
         break;
     case '3':
         system("cls");
